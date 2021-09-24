8e9f06fffb148e46a9e7906cdff462cf7b1c25af
Lucas Hlaing <minzin.hlaing@gmail.com>
2021-09-24 19:52:33 +0630

    fixed comments in main.js

--- main.js
+++ main.js
@@ -16,51 +16,6 @@ client.commands = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 });
 
-// const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-
-// for (const file of commandFiles) {
-//     const command = require (`./commands/${file}`);
-//     client.commands.set(command.name, command);
-// }
-
-
-// client.once('ready' , () => {
-//     console.log('YEE is up and running');
-//     let activities = [`Anime`, `Anime`, `Anime`   ],i = 0;
-//   setInterval(() => client.user.setActivity(`${activities[i++ %  activities.length]}`,  {type:"WATCHING"}), 5000)
-//   client.user.setStatus('idle');
-
-// });
-
-// client.on('message',message => {
-//     if(!message.content.startsWith(prefix) || message.author.bot) return
-//     const blocked = ['425258904688263188'];
-//     if(blocked.includes(message.author.id)){
-//         message.reply('no');
-//         return;
-//     };
-
-//     const arg = message.content.slice(prefix.length).split(/ +/);
-//     const command = arg.shift().toLowerCase();
-
-//     if(!client.commands.has(command)) return;
-
-//     if(command == 'ping'){
-//         client.commands.get(command).execute(message);
-//         return;
-//     }
-//     try{
-//         client.commands.get(command).execute(message, arg);
-//     }catch (error){
-//         console.log(error);
-//         message.reply('could not find that command.');
-//     }
-
-// });
-
-
-
-
 
 
 
